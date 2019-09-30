e7f3f19b0bf4fc658125af30e76cfef3174d2117
Serg Sidorov <SLSidorov@gmail.com>
2019-09-30 22:23:51 +0300

    cvbxxcb
    xcvxxcvb
    
    Co-Authored-By: xcvb <xcvb@users.noreply.github.com>

---
+++ ui/src/app/ui-service.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable } from 'rxjs';
+import * as url from "url";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UiServiceService {
+  private url: string;
+
+  constructor(private http: HttpClient) {
+    this.url = 'http://localhost:8080/hello';
+  }
+  public findAll(): Observable<Object>{
+    return this.http.get(this.url);
+  }
+
+  // public save(user:User){
+  //   return this.http.post(this.url, user);
+  // }
+}
